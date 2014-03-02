91642686a3f482f04d158bd397e1d20f4e3cd49a
Ivan Trapeznikov <trapeznikoff@yandex.ru>
2014-03-02 12:52:25 +0400

    user model created

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from users.models import User
 
-# Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    fields = ['username', 'password', 'first_name', 'last_name', 'email']
+
+
+admin.site.register(User,UserAdmin)
