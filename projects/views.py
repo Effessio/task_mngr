05ca459b1dd39c26319eac8109df4add042335c5
effessio <trapeznikoff@yandex.ru>
2014-03-26 11:01:27 +0400

    added pagination on project page

--- projects/views.py
+++ projects/views.py
@@ -17,14 +17,22 @@ def index(request):
         projects = paginator.page(1)
     except EmptyPage:
         projects = paginator.page(paginator.num_pages)
-    result_task_list = Task.objects.filter(assigned_id=user.id).order_by('due_date')[:5]
-    return render(request, 'projects/index.html', {'projects': projects, 'tasks': result_task_list})
+    task_list = Task.objects.filter(assigned_id=user.id).order_by('due_date')[:5]
+    return render(request, 'projects/index.html', {'projects': projects, 'tasks': task_list})
 
 
 @login_required()
 def project_page(request, project_id):
     project = get_object_or_404(Project, pk=project_id)
     task_list = Task.objects.filter(project__id=project_id)
+    paginator = Paginator(task_list, 5)
+    page = request.GET.get('page')
+    try:
+        task_list = paginator.page(page)
+    except PageNotAnInteger:
+        task_list = paginator.page(1)
+    except EmptyPage:
+        task_list = paginator.page(paginator.num_pages)
     members_list = User.objects.filter(projectmembership__project_id=project.id)
     return render(request, 'projects/project_page.html', {'task_list': task_list, 'members_list': members_list})
 
